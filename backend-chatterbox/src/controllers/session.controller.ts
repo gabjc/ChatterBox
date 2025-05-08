6c854e6ad257e9d4a84c4b89f08bc7a8557fb18e
gabjc <gabtez23@gmail.com>
2025-05-08 01:01:35 -0400

    rid of some of the @ts-ignores

--- backend-chatterbox/src/controllers/session.controller.ts
+++ backend-chatterbox/src/controllers/session.controller.ts
@@ -7,8 +7,6 @@ import catchErrors from "../utils/catchErrors";
 export const getSessionsHandler = catchErrors(async (req, res) => {
 	const sessions = await SessionModel.find(
 		{
-			// once again index.d.ts is not being detected by ts so we will ignore it
-			// @ts-ignore
 			userId: req.userId,
 			expiresAt: { $gt: new Date() },
 		},
@@ -24,8 +22,6 @@ export const getSessionsHandler = catchErrors(async (req, res) => {
 	return res.status(OK).json(
 		sessions.map((session) => ({
 			...session.toObject(),
-			// once again index.d.ts is not being detected by ts so we will ignore it
-			// @ts-ignore
 			...(session.id === req.sessionId && {
 				isCurrent: true,
 			}),
@@ -37,8 +33,6 @@ export const deleteSessionsHandler = catchErrors(async (req, res) => {
 	const sessionId = z.string().parse(req.params.id);
 	const deleted = await SessionModel.findOneAndDelete({
 		_id: sessionId,
-		// once again index.d.ts is not being detected by ts so we will ignore it
-		// @ts-ignore
 		userId: req.userId,
 	});
 	appAssert(deleted, NOT_FOUND, "Session not found");
--- backend-chatterbox/src/controllers/user.controller.ts
+++ backend-chatterbox/src/controllers/user.controller.ts
@@ -4,8 +4,6 @@ import appAssert from "../utils/appAssert";
 import catchErrors from "../utils/catchErrors";
 
 export const getUserHandler = catchErrors(async (req, res) => {
-	// again index.d.ts is not being detected by ts so we will ignore it
-	//@ts-ignore
 	const user = await UserModel.findById(req.userId);
 	appAssert(user, NOT_FOUND, "User not found");
 	return res.status(OK).json(user.omitPassword());
